321f75eaeb868347181be8fb14805adc265107c3
cody alvarez <codemk13@outlook.com>
2020-06-18 00:23:50 -0400

    cli

--- lib/us_aircraft/cli.rb
+++ lib/us_aircraft/cli.rb
@@ -42,25 +42,42 @@ class UsAircraft::CLI
     @aircraft = UsAircraft::Aircraft.all
     @aircraft.each_with_index.map do |index, aircraft|
       "#{index}. #{aircraft}"
-      puts "Choose an index of the aircraft to get more information, menu to return to the main menu or exit to exit the program."
-      # get_more_info_for_aircraft
+      get_more_info_for_aircraft
+    end
+  end
+
+
+  def get_more_info_for_aircraft
+    puts "Choose the index of an aircraft to get more information, menu to return to the main menu or exit to exit the program."
+    UsAircraft::Scraper.more_info_for_aircraft(@aircraft)
+    @more_info = UsAircraft::Aircraft.all
+    case user_input
+    when 'index'
+      @more_info
+    when 'menu'
+      main_menu
+    when 'exit'
+      goodbye
+    else
+      puts "Not a valid option. No surrender ! Try again !"
     end
   end
 end
 
-#   def get_more_info_for_aircraft
-#     # chosen_aircraft = gets.strip.to_i
-#     chosen_aircraft = user_input
-#     list_aircraft(chosen_aircraft) if valid_input(chosen_aircraft, @aircraft)
-#     if user_input == "exit"
-#       exit!
-#     elsif
-#       user_input == "menu"
-#       main_menu
-#     else
-#       puts "Not a valid option."
-#     end
-#   end
+  # def get_more_info_for_aircraft
+  #   # chosen_aircraft = gets.strip.to_i
+  #   UsAircraft::Scraper.get_more_info_for_aircraft
+  #   chosen_aircraft = user_input
+  #   list_aircraft(chosen_aircraft) if valid_input(chosen_aircraft, @aircraft)
+  #   if user_input == "exit"
+  #     exit!
+  #   elsif
+  #     user_input == "menu"
+  #     main_menu
+  #   else
+  #     puts "Not a valid option."
+  #   end
+  # end
 
 #   def valid_input(input, data)
 #     input.to_i <= data.length && input.to_i > 0
@@ -73,10 +90,6 @@ end
 # #     puts "#{aircraft.contractor} - #{aircraft.service} - #{aircraft.armament} - #{aircraft.power_plant} - #{aircraft.speed} - #{aircraft.range}"
 # #     end
 # #   end
-
-  # def goodbye
-  #   puts "Goodbye! Have a nice day!"
-  # end
 
 # end
 
