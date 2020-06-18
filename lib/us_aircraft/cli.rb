bfee65e76b71aedca84e3fcf9a56404c625828cc
cody alvarez <codemk13@outlook.com>
2020-06-17 20:21:54 -0400

    more cli work

--- lib/us_aircraft/cli.rb
+++ lib/us_aircraft/cli.rb
@@ -2,63 +2,66 @@ require "pry"
 class UsAircraft::CLI
   def call
     puts "\nTop US Attack Aircraft.\n"
-    menu
-    list
-    # get_more_info_for_aircraft
+    main_menu
   end
 
-  def menu
-    puts "Type 'list' to see aircraft or 'exit' to exit program."
+  def user_input
     user_input = gets.strip
+  end
+
+  def sleep_counter
+    sleep(1.0)
+    puts "."
+    sleep(1.0)
+    puts ".."
+    sleep(1.0)
+    puts "..."
+    sleep(1.0)
+  end
+
+  def main_menu
+    puts "Type 'list' to see aircraft or 'exit' to exit program."
 
     if user_input == "list"
-      list
+      puts "Sure thing, loading database."
+      # sleep_counter
+      list_aircraft    
     else 
       "No surrender ! Try again !"
-      menu
+      main_menu
     end
   end
 
-  def list
-    # puts '~ Choose an index of the aircraft to get more information. ~'
-    # list_aircraft = UsAircraft::Scraper.scrape_aircraft
-    # list_aircraft.map.with_index(1) do |aircraft, index|
-    #   "#{index}. #{aircraft}"
-    # end
-    puts "Choose an index of the aircraft to get more information."
+  def list_aircraft
+    puts "Lighting speeds, hundreds of succesful missions, the greatest US Attack Aircraft to date."
     UsAircraft::Scraper.scrape_aircraft
     @aircraft = UsAircraft::Aircraft.all
     @aircraft.each_with_index.map do |index, aircraft|
       "#{index}. #{aircraft}"
+
+    puts "Choose an index of the aircraft to get more information."
+    get_more_info_for_aircraft
     end
   end
 
+  def get_more_info_for_aircraft
+    chosen_aircraft = gets.strip.to_i
+    list_aircraft(chosen_aircraft) if valid_input(chosen_aircraft, @aircraft)
+  end
 
-    
+  def valid_input(input, data)
+    input.to_i <= data.length && input.to_i > 0
+  end
+
+  def about_aircraft
+    aircraft = @aircraft[chosen_aircraft - 1]
+    puts "AIRCRAFT INTEL FOR #{aircraft}."
    
+    Flight::Aircraft.all.each.with_index(1) do |aircraft|
+      puts "#{aircraft.contractor} - #{aircraft.service} - #{aircraft.armament} - #{aircraft.power_plant} - #{aircraft.speed} - #{aircraft.range}"
     
- 
+    end
+
+  end
+
 end
-
-#   def get_more_info_for_aircraft
-#     chosen_aircraft = gets.strip.to_i
-#     list_aircraft(chosen_aircraft) if valid_input(chosen_aircraft, @aircraft)
-#   end
-
-#   def valid_input(input, data)
-#     input.to_i <= data.length && input.to_i > 0
-#   end
-
-#   def about_aircraft
-#     aircraft = @aircraft[chosen_aircraft - 1]
-#     puts "AIRCRAFT INTEL FOR #{aircraft}."
-   
-#     Flight::Aircraft.all.each.with_index(1) do |aircraft|
-#       puts "#{aircraft.contractor} - #{aircraft.service} - #{aircraft.armament} - #{aircraft.power_plant} - #{aircraft.speed} - #{aircraft.range}"
-    
-#     end
-
-#   end
-
-
-# end
