775cd1c9ecabe9600ee172ec8adcdcb47849f85e
cody alvarez <codemk13@outlook.com>
2020-06-23 13:02:55 -0400

    cli work

--- lib/us_aircraft/cli.rb
+++ lib/us_aircraft/cli.rb
@@ -33,7 +33,7 @@ class UsAircraft::CLI
   end
 
   def list_aircraft
-    puts "Lighting speeds, hundreds of succesful missions, the greatest US Attack Aircraft to date."
+    puts "Lighting speeds, countless succesful missions to date, the greatest US Attack Aircraft to date:"
     @aircraft = UsAircraft::Aircraft.all
     @aircraft.each.with_index(1) do |aircraft, index|
       puts "#{index}. #{aircraft.name}"
@@ -45,7 +45,6 @@ class UsAircraft::CLI
   puts "Choose an aircraft by index for more information."
   index = gets.strip.to_i #sintrg to an integer to_i
   aircraft = UsAircraft::Aircraft.all[index - 1]
-  # puts "AIRCRAFT INTEL FOR #{aircraft.name}."
   case index
     when 1..2
       puts "AIRCRAFT INTEL FOR #{aircraft.name}."
@@ -57,6 +56,7 @@ class UsAircraft::CLI
       main_menu
     else
       puts "Not a valid option. No compromise ! No surrender ! Try again !"
+      main_menu
     end
   end
        
